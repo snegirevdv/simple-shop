8a3b52cbc98418e543fb2b429bb914eccc51059f
Denis Snegirev <snegirev.dv@gmail.com>
2024-11-12 13:43:56 +0300

    update admin panel

--- src/categories/admin.py
+++ src/categories/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "category")
+    list_filter = ("category",)
